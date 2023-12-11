2271410cd3d9ad68fc4d3c2073eb61f9d13e9eef
Marilise Morona <marymorona@gmail.com>
2023-12-11 09:53:35 -0300

    Update home

--- rickyAndMorty/src/pages/Home/index.tsx
+++ rickyAndMorty/src/pages/Home/index.tsx
@@ -1,7 +1,23 @@
-import react from 'react';
-import { ButtonContainer, Container, FooterContainer, ListContainer, SubtitleStyled, TitleStyled } from './styles';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { ButtonContainer, Container, FooterContainer, SubtitleStyled, TitleStyled } from './styles';
+import ListItems from './ListItems';
+import API from '../../service/service';
 
-function Home( ){
+
+const Home: React.FC = ({ navigation }) => {
+    const [apiData, setApiData] = React.useState<any>(null);
+    React.useEffect(() => {
+        // Fetch data when the component mounts
+        const fetchData = async () => {
+          const result = await API();
+          setApiData(result);
+        };
+    
+        fetchData();
+      }, []);
+
     return(
         <Container>
             <FooterContainer>
@@ -11,13 +27,15 @@ function Home( ){
                 <SubtitleStyled>
                     Search for Ricky & Morty character by name and using filters.
                 </SubtitleStyled>
-                <ButtonContainer>
-                    
-                </ButtonContainer>
+                <TouchableOpacity>
+                    <ButtonContainer>
+                        <MaterialCommunityIcons name="magnify" size={24} color="#000" />
+                    </ButtonContainer>
+                </TouchableOpacity>
             </FooterContainer>
-            <ListContainer>
-
-            </ListContainer>
+            <ListItems 
+            navigation={navigation} 
+            data = {apiData}/>
         </Container>
     )
 }
--- rickyAndMorty/src/pages/Home/styles.ts
+++ rickyAndMorty/src/pages/Home/styles.ts
@@ -2,44 +2,39 @@ import { styled } from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: #bbb765;
+    background-color: #fafafa;
     padding-top: 32px;
+    position: relative;
 `;
 
 export const FooterContainer = styled.View`
-    flex: 1;
-    background-color: #bb8765;
-    padding-top: 32px;
+    flex: 0.2;
+    background-color: #fafafa;
     padding-bottom: 32px;
 `;
 
-export const ListContainer = styled.View`
-    flex: 1;
-    background-color: #bb2344;
-    padding-top: 32px;
-`;
-
 export const TitleStyled = styled.Text`
-    text-align: center;
+    text-align: left;
+    padding-left: 50px;
     font-weight: bold;
-    font-size: 35;
+    font-size: 35px;
 `;
 
 export const SubtitleStyled = styled.Text`
-    text-align: center;
+    text-align: left;
+    padding-left: 20px;
     padding-top: 10px;
     padding-left: 26px;
     padding-right: 26px;
-    padding-bottom: 30px;
+    padding-bottom: 26px;
     color: #bbbbbb;
 `;
 
-export const ButtonContainer = styled.View`
+export const ButtonContainer = styled.TextInput`
     padding-top: 12px;
     padding-bottom: 12px;
-    width: 300px;
-    align-items: center;
-    justify-content: center;
+    margin-left: 20px;
+    margin-right: 20px;
     border-color: #056d10;
     border-width: 1px;
     border-top-left-radius: 10px;
@@ -47,3 +42,4 @@ export const ButtonContainer = styled.View`
     border-bottom-left-radius: 10px;
     border-bottom-right-radius: 10px;
 `;
+
